1f21b40bbd80853ed3654d03fce50f43f28b3783
Matheus <matheusbispo0404@gmail.com>
2025-04-14 09:52:07 -0300

    funções, let e var

--- src/js/script.js
+++ src/js/script.js
@@ -15,20 +15,56 @@
 
 //JOGO DA ADVINHAÇÃO
 
-let palpite;
-const sorteio =Math.floor(Math.random()* 10)+1;
+// let palpite;
+// const sorteio =Math.floor(Math.random()* 10)+1;
 
-do{
-    palpite = parseInt(prompt("Escolha um número entre 1 e 10"));
-    if(palpite !== sorteio){
-        alert('tente novamente')
+// do{
+//     palpite = parseInt(prompt("Escolha um número entre 1 e 10"));
+//     if(palpite !== sorteio){
+//         alert('tente novamente')
+//     }
+// }while(palpite !== sorteio)
+
+// alert(`parabéns você ganhou ${palpite}`)
+
+// // FUNÇÕES
+
+// function saudacao(nome){
+//     console.log("seja bem vindo",nome)
+// }
+// saudacao("fiap")
+
+// function semafaro(){
+//     let sinal1="Amarelo"
+//     let sinal2="vermelho"
+//     let sinal3="verde"
+//     alert(`O semafaro ${sinal1}`)
+//     alert(`O semafaro ${sinal2}`)
+//     alert(`O semafaro ${sinal3}`)
+// }
+// semafaro();
+
+//VAR -> consegue acessar fora do bloco de manipulação
+
+
+
+// function exemplo(){
+//     if(true){
+//         var num=100;
+//         console.log("Dentro do bloco if",num)
+//     }
+//     console.log("Dentro do bloco da função",num)
+// }
+// exemplo();
+
+/*CRIE UM PROGRAMA QUE VERIFICA SE O NÚMERO É PAR*/
+
+function verificaPar(numero){
+    if(numero % 2 === 0){
+        console.log("o numero é par")
     }
-}while(palpite !== sorteio)
-
-alert("parabéns você ganhou ${palpite}")
-
-
-
-
-
-
+    else{
+        console.log("o número é impar")
+    }
+}
+verificaPar(0);
